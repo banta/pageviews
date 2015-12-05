090b620458a0d4e3fbb1ca9df4fc063e57db3aed
Bernard Banta <banta.bernard@gmail.com>
2015-12-05 21:03:20 +0300

    Added an engine file for the gem

---
+++ lib/pageviews/engine.rb
@@ -0,0 +1,15 @@
+require 'pageviews'
+require 'rails'
+require 'active_record'
+# require 'active_support'
+
+module Pageviews
+  class Engine < Rails::Engine
+    initializer 'pageviews.controller' do
+      ActiveSupport.on_load(:action_controller) do
+        include PageviewsController::InstanceMethods
+        extend PageviewsController::ClassMethods
+      end
+    end
+  end
+end
