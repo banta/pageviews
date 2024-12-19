8ffa74e5ad58246dadebe21db35a326aad39a098
Derek Neighbors <dneighbo@gmail.com>
2024-12-19 13:09:37 -0700

    PWN-2569 fix timestamp migration to be rails 8 compatiable

--- lib/generators/pageviews/pageviews_generator.rb
+++ lib/generators/pageviews/pageviews_generator.rb
@@ -5,13 +5,8 @@ class PageviewsGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   source_root File.join(File.dirname(__FILE__), 'templates')
 
-  def self.next_migration_number(dirname)
-    sleep 1
-    if ActiveRecord::Base.timestamped_migrations
-      Time.now.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
-    end
+  def next_migration_number(dirname)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
   end
 
   def create_migration_file
